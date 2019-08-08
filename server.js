300f4c6166ae738d2a8a2bebedb5b927be693528
jiangminxi <jiangminxi@banggood.com>
2019-08-08 17:19:05 +0800

    目录

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-06-28 09:49:58
+ * @LastEditTime: 2019-07-26 12:30:24
+ * @LastEditors: Please set LastEditors
+ */
 // 引入相关模块
 var http = require('http');
 var url = require('url');
 var path = require('path');
 var readStaticFile = require('./modules/readStaticFile');
 const CONFIG = require('./config');
-const POST = 9526;
+const POST = 8091;
 
 // 搭建 HTTP 服务器
 var server = http.createServer(function(req, res) {
